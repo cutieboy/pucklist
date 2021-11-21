98416831556af50117327cfb690e324fb906dc81
zachj95 <zachariejones@gmail.com>
2021-11-21 13:28:39 -0800

    Added dependencies for passportJS

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express')
+const session = require('express-session')
+const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
+const passport = require('passport')
+const localStrategy = require('passport-local').Strategy
+const bcrypt = require('bcrypt')
 const cors = require('cors')
 require('dotenv/config')
 
