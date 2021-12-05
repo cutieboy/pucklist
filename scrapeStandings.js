29ac0a792d1f295f1075d96928cc6aa065f252c2
zachj95 <zachariejones@gmail.com>
2021-12-04 16:28:15 -0800

    Finished scrapeStandings

--- scrapeStandings.js
+++ scrapeStandings.js
@@ -5,8 +5,12 @@ async function fetchStats() {
     let URL = 'https://stats.sharksice.timetoscore.com/display-stats.php?league=1'
     let API = 'http://localhost:5000/api/standings'
 
+    await fetch(API, {
+        method: 'DELETE'
+    })
+
     const browser = await puppeteer.launch({
-        headless: false,
+        headless: true,
         defaultViewport: null,
     })
 
@@ -23,10 +27,13 @@ async function fetchStats() {
         '.collaptable > tbody:nth-child(1) > tr:nth-child(103)',
         '.collaptable > tbody:nth-child(1) > tr:nth-child(104)',
         '.collaptable > tbody:nth-child(1) > tr:nth-child(105)',
+        '.collaptable > tbody:nth-child(1) > tr:nth-child(106)',
+        '.collaptable > tbody:nth-child(1) > tr:nth-child(107)',
+        '.collaptable > tbody:nth-child(1) > tr:nth-child(108)',
     ]
 
     let teamArray = []
-    let statObjects = []
+    let teamObjects = []
 
     const getTeamData = async(teamSelector) => {
         const tables = await page.$$eval(teamSelector,
@@ -41,12 +48,39 @@ async function fetchStats() {
                 return arr
             })
         )
-        teamArray.push(tables[0])
-        console.log(teamArray)
+        return tables[0]
     }
 
-    teams.forEach((team) => {
-        getTeamData(team)
+    teams.forEach(async (team, i) => {
+        const teamData = await getTeamData(team)
+        const teamObject = {
+            team: teamData[0],
+            gp: teamData[1],
+            w: teamData[2],
+            l: teamData[3],
+            t: teamData[4],
+            otl: teamData[5],
+            points: teamData[6],
+            streak: teamData[7],
+            tieBreaker: teamData[8]
+        }
+
+        try {
+            const response = await fetch('http://localhost:5000/api/standings', {
+                method: 'POST',
+                body: JSON.stringify(teamObject),
+                headers: { 'Content-Type': 'application/json' }
+            })
+            const data = await response.json()
+            console.log(data)
+        } catch(err) {
+            console.log({message: err})
+        }
+        
+        if(i === teams.length - 1) {
+            console.log(teamObjects)
+            await browser.close()
+        }
     })
 
 
@@ -81,18 +115,18 @@ async function fetchStats() {
     // })
 
     // statObjects.forEach(async (stat) => {
-    //     try {
-    //         const response = await fetch('http://localhost:5000/api/stats', {
-    //             method: 'POST',
-    //             body: JSON.stringify(stat),
-    //             headers: { 'Content-Type': 'application/json' }
-    //         })
+        // try {
+        //     const response = await fetch('http://localhost:5000/api/stats', {
+        //         method: 'POST',
+        //         body: JSON.stringify(stat),
+        //         headers: { 'Content-Type': 'application/json' }
+        //     })
 
-    //         const data = await response.json()
-    //         console.log(data)
-    //     } catch(err) {
-    //         console.log({message: err})
-    //     }
+        //     const data = await response.json()
+        //     console.log(data)
+        // } catch(err) {
+        //     console.log({message: err})
+        // }
     // })
 
 }
